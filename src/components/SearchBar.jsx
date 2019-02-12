92c5e847c404c83f6b36451c655f83842f50893f
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-11 16:54:02 -0800

    added additional state to keep track of and inputs

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "" };
+  state = { address: "", city: "", state: "", zipcode: "" };
 
  onFormSubmit = (e) => {
         e.preventDefault();
-        this.props.onSearchSubmit(this.state.term)
+        this.props.onSearchSubmit(this.state.address, this.state.city, this.state.state, this.state.zipcode)
     }
 
   render() {
@@ -13,12 +13,33 @@ class SearchBar extends React.Component {
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field" >
-            <label>Image Search</label>
+            <h1>Type in your address to see our valuation</h1>
+            <label>Street Address: include suffix (dr, ave, st, etc)</label>
             <input
               type="text"
-              value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              value={this.state.address}
+              onChange={e => this.setState({ address: e.target.value })}
             />
+            <label>City</label>
+            <input
+              type="text"
+              value={this.state.city}
+              onChange={e => this.setState({ city: e.target.value })}
+            />
+            <label>State Abbreviation (CA, NY, etc)</label>
+            <input
+              type="text"
+              value={this.state.state}
+              onChange={e => this.setState({ state: e.target.value })}
+            />
+            <label>Zipcode</label>
+            <input
+              type="text"
+              value={this.state.zipcode}
+              onChange={e => this.setState({ zipcode: e.target.value })}
+            />
+            <button onClick={this.onFormSubmit}>Search</button>
+       
           </div>
         </form>
       </div>
